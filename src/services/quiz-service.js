0e2d460761ac462049cf945b99f2892bb2f8ed2a
Pablo Forero <ps.gomezforero@gmail.com>
2021-04-18 22:43:09 -0400

    Added attempt api to quiz-service

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -8,10 +8,26 @@ export const findQuizById = (quizId) =>
   fetch(`${QUIZZES_URL}/${quizId}`)
     .then(response => response.json())
 
+export const getAttemptsForQuiz = (quizId) =>
+  fetch(`${QUIZZES_URL}/${quizId}/attempts`)
+    .then(response => response.json())
+
+export const submitQuiz = (quizId, questions) => {
+  fetch(`${QUIZZES_URL}/${quizId}/attempts`, {
+    method: 'POST',
+    body: JSON.stringify(questions),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json())
+    .then(result =>console.log(result));
+}
 
 const api = {
   findAllQuizzes,
   findQuizById,
+  getAttemptsForQuiz,
+  submitQuiz
 };
 
 export default api;
